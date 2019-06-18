6f84153b2b39a34293f04c420497a6f4a5ca8eaa
Sebastian Vivten <n0l0cale@gmail.com>
2019-06-18 01:12:34 +0000

    moving from single python file to config.json variant

--- README.md
+++ README.md
@@ -13,6 +13,29 @@ sudo apt-get install python python-pip -y
 pip install -r requirements.txt
 ```
 
+Adjust the configfile (config.json) to your environment:
+Insert the name of your camera and its ID here
+to find this id: enable RTSP in your NVR Settings page, save that setting and return to your camera overview
+now when you select a camera you see a new option "RTSP Service", where the RTSP URL is being displayed like rtsp://192.168.178.100:7447/5a71234567891_0 where 5a71234567891 is the ID you are looking for
+
+Please use the config.json file as provided in the file repository - not the version below - json is not intended to be commented...
+```
+{
+    "unifiserver": "controller.domain.tld", # your IP or FQDN of your NVR
+    "unifiadminkey": "xx-your-api-key-xx", # this API key you find in the settings of your NVR
+    "cameras": [
+        {
+           "name": "camera1", # this is the name of the camera1
+           "id": "5a71234567891" # this is the id you have evaluated
+        },
+        {
+            "name": "camera2", # this is the name of the camera2
+            "id": "5a71234567892" # this is the id you have evaluated
+        }
+   ]
+}
+```
+
 How to use this program:
 "python ubinty.py camera1 false" to disable recording
 "python ubinty.py camera1 true" to enable recording
--- ubinty.py
+++ ubinty.py
@@ -11,23 +11,34 @@ logging.debug("Ubinty started" + str(now))
 logging.debug("Parameter 1: " + sys.argv[1])
 logging.debug("Parameter 2: " + sys.argv[2])
 
+# LOADING CONFIG.JSON
+with open('config.json') as json_data_file:
+    data = json.load(json_data_file)
+
+CAMERA_NAME=sys.argv[1]
+CAMERA_ACTION=sys.argv[2]
+
 # MODIFY 
-UNIFI_SERVER="controller.domain.tld" # your IP or FQDN of your NVR
-UNIFI_ADMIN_APIKEY="xx-your-api-key-xx" # this API key you find in the settings of your NVR
-
-# insert the name of your camera and its ID here
-# to find this id: enable RTSP in your NVR Settings page, save that setting and return to your camera overview
-# now when you select a camera you see a new option "RTSP Service", where the RTSP URL is being displayed like rtsp://192.168.178.100:7447/5a71234567891_0 where 5a71234567891 is the ID you are looking for
-CAM_DICT={'camera1': '5a71234567891', 'camera2': '5a71234567892'}
-
+#UNIFI_SERVER="controller.domain.tld" # your IP or FQDN of your NVR
+#UNIFI_ADMIN_APIKEY="xx-your-api-key-xx" # this API key you find in the settings of your NVR
 
 # program
 CAMERA_NAME=sys.argv[1]
 CAMERA_ACTION=sys.argv[2]
 
-url = 'https://' + UNIFI_SERVER + ':7443/api/2.0/camera/' + CAM_DICT[CAMERA_NAME] + '?apiKey=' + UNIFI_ADMIN_APIKEY
-payload = {'name': CAMERA_NAME,'recordingSettings': {'motionRecordEnabled': CAMERA_ACTION, 'fullTimeRecordEnabled': False, 'channel': '0'}}
-headers = {'content-type': 'application/json'}
-response = requests.put(url, data=json.dumps(payload), headers=headers)
-logging.debug(response)
-print(response)
+found=0
+
+for i in data["cameras"]:
+    if i['name'] == CAMERA_NAME:
+        url = 'https://' + data['unifiserver'] + ':7443/api/2.0/camera/' + i['id'] + '?apiKey=' + data['unifiadminkey']
+        payload = {'name': CAMERA_NAME,'recordingSettings': {'motionRecordEnabled': CAMERA_ACTION, 'fullTimeRecordEnabled': False, 'channel': '0'}}
+        headers = {'content-type': 'application/json'}
+        response = requests.put(url, data=json.dumps(payload), headers=headers)
+        logging.debug(response)
+        print(response)
+        found=1
+        break
+
+if found is 0:
+    print "FATAL: camera-id could not be found"
+
